75d5ce3dcecd1df39e688dcc2b27b038338e5b07
Edward1217 <m932012@gmail.com>
2025-07-15 21:25:27 -0700

    fourth push

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
-import path from "path";
+import cors from "cors";
+
 dotenv.config();
 const app = express();
 app.use(cookieParser());
-
+app.use(cors());
 app.use(express.json());
+app.use(express.static("dist"));
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -19,11 +21,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-const __dirname = path.resolve();
-path.join(__dirname, "../client/dist");
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
